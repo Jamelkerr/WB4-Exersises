c91864eaa7ceee5dc5815bef4848bafda8b0c11d
jamel kerr <jamel.kerr@yahoo.com>
2024-05-07 10:20:26 -0400

    label

---
+++ demo-1-5.js
@@ -0,0 +1,8 @@
+let emp = {
+    employeeId: "1",
+    name: "Ezra",
+    jobTitle: "Theater Teacher",
+    payRate: 38.46
+    };
+
+    console.log(emp.payRate);
---
+++ demo.1-6.js
@@ -0,0 +1,20 @@
+let emp = {
+    employeeId: "1",
+    name: "Ezra",
+    jobTitle: "Theater Teacher",
+    payRate: 38.46
+    };
+
+    console.log(emp.payRate);
+
+    let emp2 = {
+        employeeId: "2",
+        name: "Elisha",
+        jobTitle: "Game Programmer",
+        payRate: 43.27
+        };
+        emp2.startDate = "2024-05-07";
+       
+        console.log(emp2.name)
+
+        console.log(emp2.startDate)
